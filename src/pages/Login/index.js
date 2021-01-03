426e90f155b00d0ce2400c9960feec659a10c593
Paulo Henrique Filipini <paulofilipini@hotmail.com>
2021-01-03 19:20:57 -0300

    criando tela Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,118 @@
-import react from 'react'
+import React from 'react';
+import * as yup from 'yup';
+import { FormikProvider, useFormik } from "formik";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from '@material-ui/core/TextField';
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Container, ContainerCarousel, Imagem, ContainerLogin, ContainerLogo, Logo, ContainerCenter, Titulo,
+    ContainerForgotButton, ForgotButton, ContainerSignUp, Sign, SignGoogle, ContainerCreateAccount, CreateAccount, ErrorMessage,
+    Line, LineText
+}
+    from './styles.js'
+import googleIcon from '../../assets/googleIcon.png'
+import Data from '../../assets/Data.png'
 
 
-export default function Login() {
+const initialValues = {
+    email: "",
+    password: "",
+}
+let validationSchema = yup.object().shape({
+    email: yup.string().email().required("*Este campo não pode ser vazio"),
+    password: yup
+        .string()
+        .required("*Este campo não pode ser vazio")
+})
+
+export default function Login(onSubmit) {
+
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit
+    });
+    const emailProps = formik.getFieldProps("email");
+    const passwordProps = formik.getFieldProps('password');
     return (
-        <h1> Hello word</h1>
+        <form onSubmit={formik.handleSubmit}>
+            <Container>
+                <MuiThemeProvider>
+                    <ContainerCarousel>
+                        <Carousel>
+                            <Carousel.Item>
+                                <Imagem
+                                    width="100%"
+                                    height='100%'
+                                    src={Data}
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Imagem
+                                    width="100%"
+                                    height='100%'
+                                    src={Data}
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Imagem
+                                    width="100%"
+                                    height='100%'
+                                    src={Data}
+                                />
+                            </Carousel.Item>
+                        </Carousel>
+                        <div style={{ padding: 50 }}>
+                            <h3 style={{ marginTop: 5, color: '#FFF', fontSize: 20 }}  >Marcenas mattis egestas</h3>
+                            <p style={{ color: '#FFF', fontSize: 12 }}>Erdum et malesuada fames ac ante ileum primmer in faucibus uspendisse porta.</p>
+                        </div>
+                    </ContainerCarousel>
+                    <ContainerLogin>
+                        <ContainerLogo>
+                            <Logo> Invision </Logo>
+                        </ContainerLogo>
+                        <ContainerCenter>
+                            <Titulo> Welcome to Invision</Titulo>
+                        </ContainerCenter>
+                        <ContainerCenter>
+                            <ContainerCenter>
+                                <TextField label="Users name or Email" type="email" name="email" InputLabelProps={{ style: { fontSize: 14 } }} {...emailProps} />
+                                {formik.touched.email && formik.errors.email ? (
+                                    <ErrorMessage>{formik.errors.email}</ErrorMessage>
+                                ) : null}
+                            </ContainerCenter>
+                            <ContainerCenter>
+                                <TextField label="Password" type="password" name="password" InputLabelProps={{ style: { fontSize: 14 } }} {...passwordProps} />
+                                {formik.touched.password && formik.errors.password ? (
+                                    <ErrorMessage>{formik.errors.password}</ErrorMessage>
+                                ) : null}
+                            </ContainerCenter>
+                            <ContainerForgotButton >
+                                <ForgotButton>  Forgot Password? </ForgotButton>
+                            </ContainerForgotButton>
+                            <ContainerSignUp>
+                                <Sign type="submit" > Sign up</Sign>
+                            </ContainerSignUp>
+                        </ContainerCenter>
+                        <Container style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <Line />
+
+                            <LineText> Or</LineText>
+                            <Line />
+                        </Container>
+                        <ContainerCenter>
+                            <SignGoogle variant="contained" startIcon={<img src={googleIcon} />}> Sign with Google</SignGoogle>
+                        </ContainerCenter>
+                        <Container>
+                            <ContainerCreateAccount>
+                                <text> New <b>Invision? </b> <CreateAccount href="cadastro"> Create Account</CreateAccount></text>
+                            </ContainerCreateAccount>
+
+                        </Container>
+                    </ContainerLogin>
+                </MuiThemeProvider>
+            </Container >
+        </form>
     )
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,8 +1,116 @@
-import react from 'react'
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
+export const Container = styled.div`
+display: flex;
+flex: 1;
+flex-direction: row;
+`;
 
-export default function  Login() {
-return (
-    <h1> Hello word</h1>
-)
+export const ContainerCarousel = styled.div`
+flex: 0.8;
+background-color: #A9C5BA
+`;
+
+export const Imagem = styled.img`
+background-color: #A9C5BA;
+ margin-top: 15px;
+  margin-left: 5px;
+`;
+
+export const ContainerLogin = styled.div`
+flex: 1;
+padding: 20px;
+`;
+
+export const ContainerLogo = styled.div`
+text-align: right;
+`;
+
+export const Logo = styled.h4`
+font-size: 20px 
+`;
+
+export const ContainerCenter = styled.div`
+text-align: center;
+`;
+
+export const Titulo = styled.h1` 
+font: normal normal normal 30px/38px Muli;
+letter-spacing: 0px;
+color: #707070;
+opacity: 1;
+  `
+
+export const Input = styled.input`
+text-align: left;
+font-size: 14px;
+font: normal normal normal 14px/18px Muli;
+letter-spacing: 0px;
+opacity: 1;
+border-width:0px;
+border:none;
+color: #707070;
+`;
+
+export const ContainerForgotButton = styled.div`
+align-Items: center;
+margin-left: 90px;
+`;
+
+export const ContainerSignUp = styled.div`
+margin-top: 30px;
+margin-bottom: 30px;
+`;
+
+export const ForgotButton = styled(Button)`
+&& {
+  font-size: 7px
+} 
+`;
+
+export const Sign = styled(Button)`
+&& {
+background: #707070 0% 0% no-repeat padding-box;
+border-radius: 100px;
+color: #ffff;
+opacity: 1;
+width: 175px;
+height: 35px;
+}`;
+
+export const SignGoogle = styled(Button)`
+&& {
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 3px 6px #00000029;
+border-radius: 100px;
+font: normal normal normal 12px/14px Lato;
+opacity: 1;
 }
+`;
+
+export const ContainerCreateAccount = styled.div`
+text-align: center; 
+width: 100%;
+ margin-top: 30px;
+`;
+
+export const CreateAccount = styled.a` 
+color: #A9C5BA;
+`;
+
+export const ErrorMessage = styled.div`
+color:red;
+font-size:12px;
+`
+
+export const Line = styled.div`
+border-bottom: 1px solid #707070;
+flex: 0.2
+`;
+
+export const LineText = styled.text`
+font-size: 12px;
+margin: 4px
+`;
