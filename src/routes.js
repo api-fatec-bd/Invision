219aa980f32f2c6435f983eed52951207bece25b
Felipe Gustavo Braga <felipemessibraga@gmail.com>
2021-12-13 22:06:27 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import Cadastro from './pages/Cadastro'
 
 export default function Routes() {
     return (
-        <BrowserRouter basename="https://api-fatec-bd.github.io/Invision/">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route path="/" exact component={Login} />
                 <Route path="/cadastro" component={Cadastro} />
